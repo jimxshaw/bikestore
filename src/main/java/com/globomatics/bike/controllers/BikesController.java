e20d593a02cbff39a73c20dba2b067624d26e07f
Jim Shaw <jimxshaw@gmail.com>
2018-11-07 20:19:21 -0600

    Add create and get by id methods.

--- src/main/java/com/globomatics/bike/controllers/BikesController.java
+++ src/main/java/com/globomatics/bike/controllers/BikesController.java
@@ -1,9 +1,8 @@
 package com.globomatics.bike.controllers;
 
 import com.globomatics.bike.models.Bike;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,5 +16,16 @@ public class BikesController {
         List<Bike> bikes = new ArrayList<>();
 
         return bikes;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void create(@RequestBody Bike bike) {
+
+    }
+
+    @GetMapping("/{id}")
+    public Bike get(@PathVariable("id") long id) {
+        return new Bike();
     }
 }
